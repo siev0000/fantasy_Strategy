67dfd994fb0b6c561d0395049c7976a323f50aa5
siev0000 <skkt36@gmail.com>
2026-09-12 10:03:50 +0900

    ui: enable responsive Phaser viewport runtime

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { installResponsivePhaserRuntime } from "./responsive-phaser-runtime.js";
 import "./styles.css";
 import "./mobile-responsive-ui.css";
 import "./v39-field-theme.css";
 
+installResponsivePhaserRuntime();
 createApp(App).mount("#app");
