6041317a1f8baf29210928e7e072fd2f00445c8c
siev0000 <skkt36@gmail.com>
2026-09-12 09:48:36 +0900

    ui: load v39 field theme after legacy styles

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./styles.css";
 import "./mobile-responsive-ui.css";
+import "./v39-field-theme.css";
 
 createApp(App).mount("#app");
