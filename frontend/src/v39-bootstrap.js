c7c1dd705226aabe7b4faaafecd5f509359eb89a
siev0000 <skkt36@gmail.com>
2026-09-13 10:08:57 +0900

    feat: load v39 land detail binding

--- frontend/src/v39-bootstrap.js
+++ frontend/src/v39-bootstrap.js
@@ -4,6 +4,7 @@ import "./v39-map-controls-layout.js";
 import "./v39-field-settings-entry.js";
 import "./v39-field-settings-final.js";
 import "./v39-field-settings-stabilizer.js";
+import "./v39-land-detail.js";
 import "./v39-research-ui.js";
 import "./v39-design-docs-viewer.js";
 import "./v39-text-input-modal.js";
