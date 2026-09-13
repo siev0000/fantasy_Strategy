26ee959c1bdf436d9a7eab4eaeeeb2cbfefeef65
siev0000 <skkt36@gmail.com>
2026-09-13 09:38:24 +0900

    feat: load map controls layout and text input modal

--- frontend/src/v39-bootstrap.js
+++ frontend/src/v39-bootstrap.js
@@ -1,7 +1,9 @@
 // The v39 HTML is the initial screen. Feature modules only attach behavior to it.
 import "./v39-field-runtime-final.js";
+import "./v39-map-controls-layout.js";
 import "./v39-field-settings-entry.js";
 import "./v39-field-settings-final.js";
 import "./v39-field-settings-stabilizer.js";
 import "./v39-research-ui.js";
 import "./v39-design-docs-viewer.js";
+import "./v39-text-input-modal.js";
