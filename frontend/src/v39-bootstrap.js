29e97bdc8fd0b797cf990236c13f51335a0d0527
siev0000 <skkt36@gmail.com>
2026-09-13 22:43:11 +0900

    feat: load v39 display and adaptive zoom controls

--- frontend/src/v39-bootstrap.js
+++ frontend/src/v39-bootstrap.js
@@ -3,6 +3,8 @@ import "./v39-operation-ui.js";
 import "./v39-legacy-ui.js";
 import "./v39-field-runtime-final.js";
 import "./v39-map-controls-layout.js";
+import "./v39-display-settings.js";
+import "./v39-camera-zoom-controller.js";
 import "./v39-field-settings-entry.js";
 import "./v39-field-settings-final.js";
 import "./v39-field-settings-stabilizer.js";
