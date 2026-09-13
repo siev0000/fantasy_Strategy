add0bd68deec29d5a46695b2cdd42f0beb0e5648
siev0000 <skkt36@gmail.com>
2026-09-13 09:04:43 +0900

    feat: wire completed v39 field settings flow

--- frontend/src/v39-bootstrap.js
+++ frontend/src/v39-bootstrap.js
@@ -1,5 +1,6 @@
 // The v39 HTML is the initial screen. Feature modules only attach behavior to it.
-import "./v39-field-runtime.js";
+import "./v39-field-runtime-final.js";
 import "./v39-field-settings-entry.js";
+import "./v39-field-settings-final.js";
 import "./v39-research-ui.js";
 import "./v39-design-docs-viewer.js";
