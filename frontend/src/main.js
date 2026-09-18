1b660b841d160df6aa177f922f991141023ac403
siev0000 <skkt36@gmail.com>
2026-09-18 13:59:24 +0900

    文字アイコンフォントをViteビルドへ同梱

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { installResponsivePhaserRuntime } from "./responsive-phaser-runtime.js";
+import "@fontsource/noto-sans-symbols-2/400.css";
+import "@fontsource/noto-sans-symbols/400.css";
+import "@fontsource/noto-color-emoji/400.css";
 import "./styles.css";
 import "./v39-field-theme.css";
 import "./fullscreen-game-shell.css";
