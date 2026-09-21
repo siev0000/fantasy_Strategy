0104ff5d17aa202e05e1a367abd6d4bcdf620360
siev0000 <skkt36@gmail.com>
2026-09-21 19:49:40 +0900

    Remove fixed movement from new units

--- frontend/src/lib/v39-unit-creation-rules.js
+++ frontend/src/lib/v39-unit-creation-rules.js
@@ -94,7 +94,6 @@ function createUnit(player, check, name, index) {
     x:village.x,
     y:village.y,
     position:[village.x, village.y],
-    movement:40,
     combatProfile:{ ...check.mode },
     settlementId:text(village.settlementId || village.id),
     equipment
